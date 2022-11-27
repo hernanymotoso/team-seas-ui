e1c05b89bd02fb49468227d33537870961e07c03
Hernany Motoso <contatohernany@gmail.com>
2022-11-27 10:33:51 -0300

    chore: code cleanup add team seas logo

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,7 @@ import {
   ChakraProvider,
   Box,
   Text,
-  Link,
-  VStack,
+ VStack,
   Code,
   Grid,
   theme,
@@ -22,15 +21,7 @@ export const App = () => (
           <Text>
             Edit <Code fontSize="xl">src/App.tsx</Code> and save to reload.
           </Text>
-          <Link
-            color="teal.500"
-            href="https://chakra-ui.com"
-            fontSize="2xl"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn Chakra
-          </Link>
+       
         </VStack>
       </Grid>
     </Box>
